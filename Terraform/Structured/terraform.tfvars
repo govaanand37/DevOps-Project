43028bb7ba7f189a831b38258837efebf31f3a1a
govaanand37 <govasana07@gmail.com>
2024-02-25 20:33:17 +0530

    Updated repo

---
+++ Terraform/Structured/terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-0c7217cdde317cfec"
+instance_type = "t2.micro"
+key_name = "terraform-test"
---
+++ Terraform/Structured/variables.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+    description = "value of ami"
+  
+}
+
+variable "instance_type" {
+    description = "value of instance type"
+  
+}
+
+variable "key_name" {
+  description = "value of key_name"
+}
